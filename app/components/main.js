0cb384d91e113bad36489d3d124f3f8305b2081c
Viacheslav Shegai <vshegai@epiphan.com>
2022-09-02 13:37:34 -0400

    fix eslint

--- app/components/main.js
+++ app/components/main.js
@@ -1,21 +1,23 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
+/*global api*/
+
 export default class MainComponent extends Component {
 
-    @tracked title = "";
-    @tracked text = "";
-    @tracked winId = undefined;
+  @tracked title = '';
+  @tracked text = '';
+  @tracked winId = undefined;
 
-    constructor(owner, args) {
-        super(owner, args);
+  constructor(owner, args) {
+    super(owner, args);
 
-        api.handle( 'notification-data', ( data ) => {
-            this.title = data.title;
-            this.text = data.text;
-            this.winId = data.id;
-        });
+    api.handle('notification-data', (data) => {
+      this.title = data.title;
+      this.text = data.text;
+      this.winId = data.id;
+    });
 
-        api.send('ready', 'ready to listen');
-    }
+    api.send('ready', 'ready to listen');
+  }
 }
