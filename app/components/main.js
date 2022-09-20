ab5ec95203d6de1875bb6f71d3cb9b30aea45cf4
Viacheslav Shegai <vshegai@epiphan.com>
2022-09-19 20:58:30 -0400

    linter

--- app/components/main.js
+++ app/components/main.js
@@ -20,17 +20,18 @@ export default class MainComponent extends Component {
         if (notification.text == notification.big_text)
           notification.big_text = null;
 
-        if (typeof notification.ongoing != 'undefined' &&
-            notification.ongoing) {
-
-            if (typeof notification.progress_indeterminate == 'undefined')
-              notification.progress_indeterminate = false;
-            if (notification.progress_indeterminate == false) {
-              if (typeof notification.progress == 'undefined')
-                notification.progress_indeterminate = true;
-                if (typeof notification.progress_max == 'undefined')
-                notification.progress_indeterminate = true;
-            }
+        if (
+          typeof notification.ongoing != 'undefined' &&
+          notification.ongoing
+        ) {
+          if (typeof notification.progress_indeterminate == 'undefined')
+            notification.progress_indeterminate = false;
+          if (notification.progress_indeterminate == false) {
+            if (typeof notification.progress == 'undefined')
+              notification.progress_indeterminate = true;
+            if (typeof notification.progress_max == 'undefined')
+              notification.progress_indeterminate = true;
+          }
         }
 
         for (idx = 0; idx < this.notifications.length; idx++)
